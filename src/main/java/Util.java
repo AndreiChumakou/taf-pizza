e44811d91ec82f634beae79a764a9a0827c1394a
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-22 11:08:50 +0300

    refactor Util.

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -1,2 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class Util {
+
+    public static void waitForPresenceByXPath(WebDriver driver, int millis, String xPath) {
+        new WebDriverWait(driver, Duration.ofMillis(millis))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xPath)));
+    }
+    public static void waitForPresenceByLinkText(WebDriver driver, int millis, String linkText) {
+        new WebDriverWait(driver, Duration.ofMillis(millis))
+                .until(ExpectedConditions.presenceOfElementLocated(By.linkText(linkText)));
+    }
+
+    public static void openCategoryMenu(WebDriver driver, String CATEGORY_MENU_TEXT_PRODUCT) {
+        waitForPresenceByLinkText(driver, 10000, CATEGORY_MENU_TEXT_PRODUCT);
+        driver.findElement(By.linkText(CATEGORY_MENU_TEXT_PRODUCT)).click();
+    }
+
+    public static void addProductToCart(WebDriver driver, String id, String kindProductXPath) {
+        waitForPresenceByXPath(driver, 10000, kindProductXPath);
+        driver.findElement(By.xpath("//button[@data-id='" + id + "']")).click();
+    }
 }
